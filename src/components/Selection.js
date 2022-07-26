6d00b5dff67d29503c7bbdc35eee8f3064425570
Sachin kumar <95627047+sachinkr7368@users.noreply.github.com>
2022-07-26 14:00:32 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React from "react";
+
+const ColourSelector = (props) => {
+  const { config, selectNextBackground } = props;
+  const { background } = config;
+  return (
+    <button
+      className={config.classname}
+      onClick={() => selectNextBackground({ background: background })}
+    >
+      {config.label}
+    </button>
+  );
+};
+export default ColourSelector;
 
